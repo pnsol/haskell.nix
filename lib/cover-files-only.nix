0b4bf2f7dda66a66c011ea07b7e10ceb374ac1f0
Samuel Evans-Powell <mail@sevanspowell.net>
2020-08-03 14:47:20 +0800

    WIP

--- lib/cover-files-only.nix
+++ lib/cover-files-only.nix
@@ -10,10 +10,6 @@ let
   # drvSources = builtins.map (d: d.src.outPath) drvs;
   # testsWithCoverage = buildWithCoverage tests;
   # checks' = runCheck testsWithCoverage;
-  drvs' = null;
-  drvSources = null;
-  testsWithCoverage = null;
-  checks' = null;
 
   projects' = builtins.map (p:
     rec {
@@ -25,20 +21,51 @@ let
   ) projects;
 
   doThis = p: ''
-    mkdir $out/share/hpc/mix/${p.name}
+    mkdir -p $out/share/hpc/mix/${p.name}
+    mkdir -p $out/share/hpc/tix/${p.name}
 
     for drv in ${lib.concatStringsSep " " ([ p.drv ] ++ p.testsWithCoverage)}; do
       # Copy over mix files
       local mixDir=$(findMixDir $drv)
-      echo "MixDir: $mixDir"
       cp -R $mixDir/* $out/share/hpc/mix/${p.name}
+    done
+
+    # Exclude test modules from tix file
+    excludedModules=('"Main"')
+    local drv=${p.drv.src.outPath}
+    # Exclude test modules
+    local cabalFile=$(findCabalFile $drv)
+    for module in $(${pkgs.cq}/bin/cq $cabalFile testModules | ${pkgs.jq}/bin/jq ".[]"); do
+      excludedModules+=("$module")
+    done
+    echo "''${excludedModules[@]}"
+
+    hpcSumCmdBase=("hpc" "sum" "--union")
+    for exclude in ''${excludedModules[@]}; do
+      hpcSumCmdBase+=("--exclude=$exclude")
+    done
+
+    for check in ${lib.concatStringsSep " " p.checks}; do
+      pushd $check/share/hpc/tix
+      
+      for tixFileRel in $(find . -iwholename "*.tix" -type f); do
+        set -x
+        mkdir -p $out/share/hpc/tix/${p.name}/$(dirname $tixFileRel)
+        cp $tixFileRel $out/share/hpc/tix/${p.name}/$tixFileRel.pre-exclude
+        
+        local hpcSumCmd=("''${hpcSumCmdBase[@]}")
+        hpcSumCmd+=("--output=$out/share/hpc/tix/${p.name}/$tixFileRel" "$tixFileRel")
+        echo "''${hpcSumCmd[@]}"
+        eval "''${hpcSumCmd[@]}"
+      done
+
+      popd
     done
   '';
 
-in stdenv.mkDerivation {
+in
+stdenv.mkDerivation {
   name = "coverage-report";
-
-  inherit checks' drvs' drvSources;
 
   phases = ["buildPhase"];
 
@@ -47,8 +74,6 @@ in stdenv.mkDerivation {
   ]);
 
   buildPhase = ''
-    mkdir -p $out/share/hpc/mix
-    mkdir -p $out/share/hpc/tix
     mkdir -p $out/share/hpc/tix/all
 
     findMixDir() {
@@ -59,9 +84,34 @@ in stdenv.mkDerivation {
       find $1 -iname "*.cabal" -print -quit
     }
 
+    # Copy over the mix and tix files for each package
     ${lib.concatStringsSep "\n" (builtins.map doThis projects')}
 
   '';
+
+    # # Generate combined tix file for all packages
+    # excludedModules=("Main")
+    # for drv in ${lib.concatStringsSep " " allSrcs}; do
+    #   # Exclude test modules
+    #   local cabalFile=$(findCabalFile $drv)
+    #   for module in $(${pkgs.cq}/bin/cq $cabalFile testModules | ${pkgs.jq}/bin/jq ".[]"); do
+    #     excludedModules+=("$module")
+    #   done
+    # done
+    # echo "''${excludedModules[@]}"
+
+    # tixFile="$out/share/hpc/tix/all/all.tix"
+    # hpcSumCmd=("hpc" "sum" "--union" "--output=$tixFile")
+    # for check in ${lib.concatStringsSep " " allChecks}; do
+    #   for tix in $(find $check -name '*.tix' -print); do
+    #     hpcSumCmd+=("$tix")
+    #   done
+    # done
+    # for exclude in ''${excludedModules[@]}; do
+    #   hpcSumCmd+=("--exclude=$exclude")
+    # done
+    # echo "''${hpcSumCmd[@]}"
+    # eval "''${hpcSumCmd[@]}"
 
     # for check in ${lib.concatStringsSep " " checks'}; do
     #   cp -R $check/share/hpc/tix/* $out/share/hpc/tix

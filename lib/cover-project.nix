ab01d797474e60b3eb2cc3dac3aa421e70469e40
Samuel Evans-Powell <mail@sevanspowell.net>
2020-08-05 17:39:02 +0800

    Start fixing projectCoverageReport

--- lib/cover-project.nix
+++ lib/cover-project.nix
@@ -30,15 +30,20 @@ stdenv.mkDerivation {
     # Create tix file with test run information for all packages
     tixFile="$out/share/hpc/tix/all/all.tix"
     hpcSumCmd=("hpc" "sum" "--union" "--output=$tixFile")
-    for report in ${lib.concatStringsSep " " coverageReports}; do
-      for tix in $(find $report -iwholename "*.tix" -type f); do
-        hpcSumCmd+=("$tix")
-      done
+
+    ${with lib; concatStringsSep "\n" (mapAttrsToList (n: package: ''
+      identifier="${package.identifier.name}-${package.identifier.version}"
+      report=${package.coverageReport}
+      tix="$report/share/hpc/tix/$identifier/$identifier.tix"
+      hpcSumCmd+=("$tix")
 
       # Copy mix and tix information over from each report
       cp -R $report/share/hpc/mix/* $out/share/hpc/mix
       cp -R $report/share/hpc/tix/* $out/share/hpc/tix
-    done
+    '') project)}
+
+    # TODO insert project-wide HTML page here
+
     eval "''${hpcSumCmd[@]}"
   '';
 }
